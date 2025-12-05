792ac4b60139f5488288bec1abd8cc5f972a24bc
shu030929 <shu030929@gmail.com>
2025-12-05 17:32:50 +0900

    feat:보유수량

--- Assets/Scripts/StockScripts/StockTradePanel.cs
+++ Assets/Scripts/StockScripts/StockTradePanel.cs
@@ -64,7 +64,7 @@ public class StockTradePanel : MonoBehaviour
 
         if (sid == null)
         {
-            if (selectedNameText) selectedNameText.text = "거지증권사";
+            if (selectedNameText) selectedNameText.text = "거지주식";
             if (currentPriceText) currentPriceText.text = "100";
             if (totalText) totalText.text = "total";
             return;
